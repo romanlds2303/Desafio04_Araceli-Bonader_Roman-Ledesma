2568e4968b83b514c72e84dd70bbb68dc588d60b
Román Ledesma <romanledesmaa23@gmail.com>
2024-07-19 13:52:03 -0300

    Agregué el prompt con el user y password

---
+++ admin.js
@@ -0,0 +1,9 @@
+let email = 'admin@gmail.com';
+let password = '1234;'
+
+let emailUser = prompt('Introduce tu Email');
+let passwordUser = prompt('Introduce tu contraseña')
+
+if(email == emailUser && password == passwordUser){
+    alert ('Bienvenido a la plataforma');
+}
