0cdfb942a4419589576bead6ed93cfce67d8fae6
Román Ledesma <romanledesmaa23@gmail.com>
2024-07-22 16:18:52 -0300

    arreglo del alert

--- admin.js
+++ admin.js
@@ -1,8 +1,8 @@
 let email = 'admin@gmail.com';
-let password = '1234;'
+let password = '1234';
 
 let emailUser = prompt('Introduce tu Email');
-let passwordUser = prompt('Introduce tu contraseña')
+let passwordUser = prompt('Introduce tu contraseña');
 
 if(email == emailUser && password == passwordUser){
     alert ('Bienvenido a la plataforma');
